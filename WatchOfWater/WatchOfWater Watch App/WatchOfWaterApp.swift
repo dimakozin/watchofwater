ce9e38211b6de3692316e5274c904f36d4d51a44
dimakozin <dimakozin@gmail.com>
2025-08-31 17:33:18 +0300

    initial commit

---
+++ WatchOfWater/WatchOfWater Watch App/WatchOfWaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchOfWaterApp.swift
+//  WatchOfWater Watch App
+//
+//  Created by Дмитрий Козин on 31.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct WatchOfWater_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
